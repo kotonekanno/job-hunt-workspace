62167e8b1b22f5de22018fc3458f1331d075b3cb
Kotone Kanno <kotonekanno0414@hotmail.com>
2026-07-12 00:42:15 +0900

    画面構成の決定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,34 @@
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import HomePage from "./pages/HomePage";
+import CompanyListPage from "./pages/companies/CompanyListPage";
+import CompanyDetailPage from "./pages/companies/CompanyDetailPage";
+import CalendarPage from "./pages/calendar/CalendarPage";
+import TasksPage from "./pages/tasks/TasksPage";
+import EssayListPage from "./pages/essays/EssayListPage";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-500">
-      Tailwind Works!
-    </h1>
+    <>
+      <Routes>
+        {/*のちに全てMainLayoutで包む*/}
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* ここより下のページはProtectedLayoutで包む */}
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/companies" element={<CompanyListPage />} />
+        <Route path="/companies/:companyId" element={<CompanyDetailPage />} />
+
+        <Route path="/calendar" element={<CalendarPage />} />
+
+        <Route path="/tasks" element={<TasksPage />} />
+
+        <Route path="/essays" element={<EssayListPage />} />
+      </Routes>
+    </>
   );
 }
 
